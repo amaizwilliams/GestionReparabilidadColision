6c33e51dd907703908636a7917dd9fe689f6be8b
Acosta <aa6651501@gmail.com>
2026-09-03 22:25:12 -0500

    se crea la clase Tecnico vacia

---
+++ src/main/java/gestion/reparabilidad/colision/modelo/Tecnico.java
@@ -0,0 +1,10 @@
+package gestion.reparabilidad.colision.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tenico")
+public class Tecnico {
+
+}
